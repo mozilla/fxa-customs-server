fa6d11a8938c7ddbb374c820e7b5cea28d1a1724
Vijay Budhram <vbudhram@mozilla.com>
2016-09-02 11:26:13 -0400

    feat(blocklist): Add test cases for multiple lists processing

--- test/local/ip_blocklist_tests.js
+++ test/local/ip_blocklist_tests.js
@@ -2,6 +2,7 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 require('ass')
+var test = require('tap').test
 
 var log = {
   info: function () {},
@@ -9,126 +10,158 @@ var log = {
   trace: function () {}
 }
 
-var test = require('tap').test
-var IPBlocklist = require('../../lib/ip_blocklist')(log)
-var filePath = './test/mocks/simple.netset'
-
-test(
-  'calling contains without loading csv return false',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-    var result = ipBlocklist.contains('0.0.0.0')
-    t.equal(result, false, 'should not have found ip')
-    t.end()
+var config = {
+  blocklist: {
+    updatePollInterval: 1 // 1 Second
   }
-)
+}
 
-test(
-  'load ip blocklist',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
+var IPBlocklist = require('../../lib/ip_blocklist')(log, config)
+var IPBlocklistManager = require('../../lib/ip_blocklist_manager')(log, config)
 
-    ipBlocklist.load(filePath)
-      .then(function () {
-        t.end()
-      })
-      .catch(function () {
-        t.fail('Failed to load csv')
-        t.end()
-      })
+var lists = [
+  './test/mocks/firehol_level1.netset',
+  './test/mocks/simple.netset'
+]
+
+var filePath = lists[0]
+
+var commonTestCases = [
+  {
+    name: 'IPBlocklist',
+    blocklistClass: IPBlocklist,
+    list: lists[0]
+  },
+  {
+    name: 'IPBlocklistManager',
+    blocklistClass: IPBlocklistManager,
+    list: lists
   }
-)
+]
+
+commonTestCases.forEach(function (testCase) {
+  var IPBlocklist = testCase.blocklistClass
+  var filePath = testCase.list
+  var name = testCase.name
+
+  test(
+    name + ', calling contains without loading csv return false',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+      var result = ipBlocklist.contains('0.0.0.0')
+      t.equal(result, false, 'should not have found ip')
+      t.end()
+    }
+  )
+
+
+  test(
+    name + ', load ip blocklist',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          t.end()
+        })
+        .catch(function () {
+          t.fail('Failed to load csv')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns true for empty ip',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains()
+          t.equal(result, true, 'return true for empty ip')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns true for invalid ip',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains('notip')
+          t.equal(result, true, 'return true for invalid ip')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns true for ip in blocklist',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains('1.93.0.224')
+          t.equal(result, true, 'return true for found ip')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns true for ip in blocklist range',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains('0.0.0.1')
+          t.equal(result, true, 'return true for found ip')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns false for ip not in blocklist',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains('12.34.32.1')
+          t.equal(result, false, 'return false for not found ip')
+          t.end()
+        })
+    }
+  )
+
+  test(
+    name + ', returns false for ip not in blocklist range',
+    function (t) {
+      var ipBlocklist = new IPBlocklist()
+
+      ipBlocklist.load(filePath)
+        .then(function () {
+          var result = ipBlocklist.contains('3.0.0.0')
+          t.equal(result, false, 'return true for found ip')
+          t.end()
+        })
+    }
+  )
+})
 
 test(
-  'returns true for empty ip',
+  'IPBlocklist, clear blocklist',
   function (t) {
     var ipBlocklist = new IPBlocklist()
 
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains()
-        t.equal(result, true, 'return true for empty ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'returns true for invalid ip',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains('notip')
-        t.equal(result, true, 'return true for invalid ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'returns true for ip in blocklist',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains('1.93.0.224')
-        t.equal(result, true, 'return true for found ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'returns true for ip in blocklist range',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains('0.0.0.1')
-        t.equal(result, true, 'return true for found ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'returns false for ip not in blocklist',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains('12.34.32.1')
-        t.equal(result, false, 'return false for not found ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'returns false for ip not in blocklist range',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
-      .then(function () {
-        var result = ipBlocklist.contains('3.0.0.0')
-        t.equal(result, false, 'return true for found ip')
-        t.end()
-      })
-  }
-)
-
-test(
-  'clear blocklist',
-  function (t) {
-    var ipBlocklist = new IPBlocklist()
-
-    ipBlocklist.load(filePath)
+    ipBlocklist.load(lists[0])
       .then(function () {
         ipBlocklist.clear()
         t.deepEqual(ipBlocklist.ipsByPrefixLength, {}, 'empty ipsByPrefixLength object')
@@ -139,7 +172,7 @@ test(
 )
 
 test(
-  'refresh blocklist',
+  'IPBlocklist, refresh blocklist',
   function (t) {
     var ipBlocklist = new IPBlocklist()
 
@@ -152,6 +185,20 @@ test(
       })
       .catch(function (err) {
         t.fail(err)
+        t.end()
+      })
+  }
+)
+
+test(
+  'IPBlocklistManager, clear blocklist',
+  function (t) {
+    var ipBlocklist = new IPBlocklistManager()
+
+    ipBlocklist.load(lists)
+      .then(function () {
+        ipBlocklist.clear()
+        t.equal(ipBlocklist.ipBlocklists.length, 0, 'empty blocklist')
         t.end()
       })
   }
